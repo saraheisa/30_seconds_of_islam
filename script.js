4be247bd25370f4688823c3a43bfbaac1ba6d0bf
sarah eisa <sarah.ga.eisa@gmail.com>
2024-06-26 01:58:30 +0300

    feat: Replaced the use of the random ayah API with logic to randomly generate surah and ayah numbers

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const ayahTranslation = document.querySelector(".ayah-translation");
 const recitationButton = document.querySelector(".recitation-button");
 const themeToggle = document.querySelector("#theme-switch");
 
-const apiUrl = "https://api.alquran.cloud/v1/ayah/random";
 const THEME_OPTIONS = {
   Dark: "dark",
   LIGHT: "light",
@@ -12,26 +11,31 @@ const THEME_KEY = "30_seconds_of_islam_theme_selection";
 
 let currentAyahNumber = 0;
 const bitrate = 64;
-const edition = "ar.alafasy";
+const audioEdition = "ar.alafasy";
+const edition = "quran-uthmani";
+const translationEdition = "en.asad";
 
 async function getRandomAyah() {
   try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    const ayah = data.data.text;
-    const surah = data.data.surah.name;
-    const ayahNumber = data.data.numberInSurah;
-    const surahNumber = data.data.surah.number;
-    currentAyahNumber = data.data.number;
-    ayahText.innerHTML = `${ayah} (${surah} ${ayahNumber})`;
-
-    const translationResponse = await fetch(
-      `http://api.alquran.cloud/v1/ayah/${surahNumber}:${ayahNumber}/en.asad`
+    const surahNumber = Math.floor(Math.random() * 114) + 1;
+    const ayahCountResponse = await fetch(
+      `https://api.alquran.cloud/v1/surah/${surahNumber}?offset=1&limit=1`
     );
-    const translationData = await translationResponse.json();
-    const translation = translationData.data.text;
-    const surahTranslation = data.data.surah.englishName;
+    const ayahCountData = await ayahCountResponse.json();
+    const ayahCount = ayahCountData.data.numberOfAyahs;
+    const ayahNumber = Math.floor(Math.random() * ayahCount) + 1;
 
+    const response = await fetch(
+      `https://api.alquran.cloud/v1/ayah/${surahNumber}:${ayahNumber}/editions/${edition},${translationEdition}`
+    );
+    const data = await response.json();
+    const ayah = data.data[0].text;
+    const surah = data.data[0].surah.name;
+    const translation = data.data[1].text;
+    const surahTranslation = data.data[1].surah.englishName;
+
+    currentAyahNumber = data.data[0].number;
+    ayahText.innerHTML = `${ayah} (${surah} ${ayahNumber})`;
     ayahTranslation.textContent = `${translation} (${surahTranslation} ${ayahNumber})`;
   } catch (error) {
     console.log(error);
@@ -68,11 +72,12 @@ function setInitialTheme(theme) {
 }
 
 getRandomAyah();
+
 const selectedTheme = getThemeSelection();
 setInitialTheme(selectedTheme);
 
 recitationButton.addEventListener("click", () => {
-  const audioURL = `https://cdn.islamic.network/quran/audio/${bitrate}/${edition}/${currentAyahNumber}.mp3`;
+  const audioURL = `https://cdn.islamic.network/quran/audio/${bitrate}/${audioEdition}/${currentAyahNumber}.mp3`;
   const audio = new Audio(audioURL);
   audio.play();
 });
